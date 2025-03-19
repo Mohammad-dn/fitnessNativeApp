672ee2fea74af45df2b24b307ab11f079abfe1f6
Mohammad-dn <mohammaddehghani2677@gmail.com>
2025-03-19 20:13:10 +0330

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,46 @@
+import { View } from "@/components/general/Themed";
+import { useWorkouts } from "@/store";
+import { Link, router } from "expo-router";
+import { FlatList } from "react-native";
+import CustomButton from "../components/general/CustomButton";
+import WorkoutListItem from "../components/workouts/WorkoutListItem";
+
+export default function HomeScreen() {
+	const currentWorkout = useWorkouts((state) => state.currentWorkout);
+	const startWorkout = useWorkouts((state) => state.startWorkout);
+	const workouts = useWorkouts((state) => state.workouts);
+
+	const onStartWorkout = () => {
+		startWorkout();
+		router.push("/workout/current");
+	};
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				gap: 10,
+				padding: 10,
+				backgroundColor: "transparent",
+			}}
+		>
+			{currentWorkout ? (
+				<Link href="/workout/current" asChild>
+					<CustomButton title="Resume workout" />
+				</Link>
+			) : (
+				<CustomButton
+					title="Start new workout"
+					onPress={onStartWorkout}
+				/>
+			)}
+
+			<FlatList
+				data={workouts}
+				contentContainerStyle={{ gap: 8 }}
+				renderItem={({ item }) => <WorkoutListItem workout={item} />}
+				showsVerticalScrollIndicator={false}
+			/>
+		</View>
+	);
+}
